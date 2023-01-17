8a17c800adbdd5b0aacef5d71ef6b486c599d70c
Leandro Balla <leandro.balla@outlook.com>
2023-01-17 14:31:23 +1030

    Download and read

--- main.py
+++ main.py
@@ -14,5 +14,12 @@ pyautogui.press('enter')
 time.sleep(5)
 pyautogui.click(x=1208, y=521)
 time.sleep(10)
+pyautogui.hotkey('alt', 'f4')
 df = pd.read_excel(r'C:\Users\leand\Downloads\Vendas - Dez.xlsx')
-print(df)
+#print(df)
+fat = df['Valor Final'].sum()
+qtde = df['Quantidade'].sum()
+print(
+    f'O faturamento foi de R$ {fat:.2f}\n'
+    f'A quantidade de produtos vendidos foi de {qtde}'
+)
